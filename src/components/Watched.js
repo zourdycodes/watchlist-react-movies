1050112ff13da05cb3bb99c460de18d18b95036e
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-06-12 21:04:37 +0700

    finished up ---> plural words functionalities <--> there is further improvement in the future

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -10,7 +10,9 @@ const Watched = () => {
       <div className="container">
         <div className="header">
           <h1 className="heading">Watched Movies</h1>
-          <span className="count-pill">{watched.length} Movies</span>
+          <span className="count-pill">
+            {watched.length} {watched.length <= 1 ? "Movie" : "Movies"}
+          </span>
         </div>
 
         {watched.length > 0 ? (
